1469443d1f93e83014955fb5dd6e5afa6dea8894
Bianca <bianca.berghea@stud.ubbcluj.ro>
2024-03-16 22:44:55 +0200

    change date to datetime

--- src/app/apps/home/map/services/map.service.ts
+++ src/app/apps/home/map/services/map.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 import { map } from 'rxjs';
+import { DatePipe } from '@angular/common';
 
 @Injectable({
   providedIn: 'root'
@@ -9,11 +10,13 @@ export class MapService {
   url: string = 'http://localhost:8080/booking';
 
   constructor(
-    private http: HttpClient
+    private http: HttpClient, private datePipe: DatePipe
   ) { }
 
-  getDeskAvailability(id:any, date:any) {
-    return this.http.post(`${this.url}/${id}`, { date } ).pipe(map((response: any) => {
+  getDeskAvailability(id: any, date: any) {
+    var start_date = this.datePipe.transform(date, 'YYYY-MM-DD') + ' 07:00:00';
+    var end_date = this.datePipe.transform(date, 'YYYY-MM-DD') + ' 19:00:00';
+    return this.http.post(`${this.url}/${id}`, { start_date, end_date }).pipe(map((response: any) => {
       return response;
       }));
 
